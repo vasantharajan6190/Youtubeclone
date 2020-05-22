73f04e88e70d0cf27f94214b5bb10ca8a0f5ad42
vasanth6190 <vasantharajan@codingmart.com>
2020-05-22 13:43:06 +0530

    Updates

---
+++ precache-manifest.b02e106b3432eb29cc603d190192b2ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "214d74531b82726833b45692e60d3ca3",
+    "url": "/Youtubeclone/index.html"
+  },
+  {
+    "revision": "2545c243f4529c38eba6",
+    "url": "/Youtubeclone/static/css/main.af778d35.chunk.css"
+  },
+  {
+    "revision": "4ac41b0fe7351a518fdf",
+    "url": "/Youtubeclone/static/js/2.a39d2c54.chunk.js"
+  },
+  {
+    "revision": "be9831207da3d10befcd05d1ebb51a3a",
+    "url": "/Youtubeclone/static/js/2.a39d2c54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2545c243f4529c38eba6",
+    "url": "/Youtubeclone/static/js/main.28dd08fa.chunk.js"
+  },
+  {
+    "revision": "fbc591021f1121df9e91",
+    "url": "/Youtubeclone/static/js/runtime-main.434f2fa4.js"
+  },
+  {
+    "revision": "b9949b37bdafd573f0be0f4782ec299e",
+    "url": "/Youtubeclone/static/media/logo.b9949b37.jpg"
+  }
+]);
